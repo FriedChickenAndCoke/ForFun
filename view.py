afbca725f35c5ca58a1e21db05a9ace44c5c13ee
FriedChickenAndCoke <zexin_lin_wow@outlook.com>
2020-01-05 12:47:55 +0800

    创建视图login

--- view.py
+++ view.py
@@ -1,4 +1,8 @@
-from django.http import http
+from django.http import HttpResponse
+from django.shortcuts import redirect
 
+def index(request):
+    return HttpResponse('index')
 
-def
+def login(request):
+    return redirect('/index')
